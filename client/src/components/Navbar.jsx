1bd66c6f8dc7a46c9f3b04a5cc901dd5b19f0b17
Mohammad Azhar <mohammadazhar6am@gmail.com>
2025-07-11 16:12:17 +0530

    Added new favicon and increased products limit on home

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -83,7 +83,7 @@ const Navbar = () => {
       { open && (
         <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden z-99`}>
         <NavLink to="/" onClick={()=> setOpen(false)}>Home</NavLink>
-        <NavLink to='/allcategories'>All Categories</NavLink>
+        <NavLink to='/allcategories'onClick={()=> setOpen(false)}>All Categories</NavLink>
         <NavLink to="/products" onClick={()=> setOpen(false)}>All Products</NavLink>
         {user && 
         <NavLink to="/my-orders" onClick={()=> setOpen(false)}>My Orders</NavLink>
